686c9a3c509b36fd553ba0f4a9652f9f5a746965
koskei-kipkoech <patrickwayy@gmail.com>
2024-12-12 18:25:37 +0300

    updates change on relationship

---
+++ lib/migrations/versions/dfb10d6e06bc_add_game_user_association_table.py
@@ -0,0 +1,24 @@
+"""Add game_user Association Table
+
+Revision ID: dfb10d6e06bc
+Revises: 9a283e6c623e
+Create Date: 2024-12-12 18:22:01.269116
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dfb10d6e06bc'
+down_revision = '9a283e6c623e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
